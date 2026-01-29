aa1057896a19349801325673f8665af687ad074f
calizayaalex5 <calizayaalex5@gmail.com>
2026-01-29 18:27:34 -0300

    s

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,8 @@ const router = require("express").Router();
 router.use("/clientes", require("./clientes"));
 router.use("/videojuegos", require("./videojuegos"));
 
+router.get('/', (req, res) => {
+    res.send("API CRUD funcionando - Ve a /api-docs para la documentación");
+});
+
 module.exports = router;
